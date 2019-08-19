8126b7a1d45cd66db59f2e584b64e086fd810657
Chen-_-Chao <e0323902@u.nus.edu>
2019-08-19 15:08:10 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine();
+            if (input.equals("bye")) break;
+            else System.out.println(input);
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
